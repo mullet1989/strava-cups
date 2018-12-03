1a97358b5cc848bc46bc5d71cfdd7fa12570a53d
Benjamin Toomer <mullet1989@gmail.com>
2018-12-02 21:58:52 -0500

    working on production env

---
+++ src/typeorm.config.production.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from './config/config.service';
+
+@Injectable()
+export class TypeormConfigDevelopmentService implements TypeOrmOptionsFactory {
+
+  constructor(private readonly configService: ConfigService) {
+  }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      url: this.configService.get('DATABASE_URL'),
+      synchronize: true, // will create on first run
+    };
+  }
+}
