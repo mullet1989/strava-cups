60f2e8068cf10ec2ed3f3b23fd335e150b6e0f61
Benjamin Toomer <mullet1989@gmail.com>
2018-12-02 22:08:38 -0500

    addins build and production stuff

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
 import { join } from 'path';
+import { ConfigProduction } from './config.production';
 
 @Module({
   providers: [
     {
       provide: ConfigService,
-      useValue: new ConfigService(join(__dirname, '../..', `${process.env.NODE_ENV}.env`)),
+      useFactory: () => {
+        let env = process.env.NODE_ENV;
+        if (env === "development") {
+          return new ConfigService(join(__dirname, '../..', `${process.env.NODE_ENV}.env`))
+        } else {
+          return new ConfigProduction();
+        }
+      }
     },
   ],
   exports: [ConfigService],
